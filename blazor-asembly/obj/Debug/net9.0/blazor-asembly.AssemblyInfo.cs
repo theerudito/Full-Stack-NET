4bce034ca928580ba9694756ca440dfa3ebc7c2d
theerudito <erudito.tv@gmail.com>
2025-03-25 09:25:25 -0500

    v1

--- blazor-asembly/obj/Debug/net9.0/blazor-asembly.AssemblyInfo.cs
+++ blazor-asembly/obj/Debug/net9.0/blazor-asembly.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("blazor-asembly")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d23d6b620e0955ce417c69f8dd3f165492054b2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f246e73906bf2cfc4b84b05f6ea6296664be5bb")]
 [assembly: System.Reflection.AssemblyProductAttribute("blazor-asembly")]
 [assembly: System.Reflection.AssemblyTitleAttribute("blazor-asembly")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
